b2039c104bea9e0759e89d5b7d19e8ff5f5412eb
rizMehdi <ccmeall@gmail.com>
2025-02-24 13:50:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 def main():
-    st.title("IRESHA Sharecode")
-    st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
-    
-    st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
-    
-    st.subheader("For applicants")
-    st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
-    st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
-    
     # Create a container to manage visibility of content
     content_container = st.container()
+    
+    with content_container:
+        st.title("IRESHA Sharecode")
+        st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
+
+        st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
+        
+        st.subheader("For applicants")
+        st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
+        st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
     
     if st.button("Check eligibility and create sharecode"):
         # Clear the previous content
@@ -30,3 +31,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+    
