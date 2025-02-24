80c351cf268094b007fd68e97c6b9750d91a6cb4
rizMehdi <ccmeall@gmail.com>
2025-02-24 13:37:07 +0000

    Update app.py with tooltip

--- app.py
+++ app.py
@@ -11,14 +11,13 @@ def main():
     st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
     
     if st.button("Check eligibility and create sharecode"):
-        option = st.radio("Are you any of the following?", [
+        option = st.radio("Are you any of the following?", 
             "British Citizen",
             "Irish Citizen",
-            "Commonwealth Citizen ❓",
+            "Commonwealth Citizen", help="SCUBA: Self Contained Underwater Breathing Aparatus"
             "Diplomat or their family member based in the UK",
             "None of the above"
-        ], help="A Commonwealth citizen is a citizen of a Commonwealth of Nations member state. Check here if your country is a commonwealth member: https://thecommonwealth.org/our-member-countries")
-        
+                         )
         st.write(f"You selected: {option}")
     
 if __name__ == "__main__":
